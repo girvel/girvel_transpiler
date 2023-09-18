44e68804c5f41ad8c942909af2253458cf7b3b90
girvel <widauka@ya.ru>
2023-09-18 11:45:52 +0300

    Initial

---
+++ girvel.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+from lark import Lark
+
+
+parser = Lark(Path("girvel.lark").read_text())
+
+if __name__ == '__main__':
+    print(parser.parse(Path("demo.grv").read_text()))
